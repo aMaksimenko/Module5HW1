b5fc13053b167353a7dbbea148a22dfa26b3846b
amaksimenko <a.maksimenko@intetics.com>
2021-11-13 22:03:14 +0200

    UPD blank

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
-
-namespace HomeWork
+﻿namespace HomeWork
 {
     internal class Program
     {
         private static void Main()
         {
+            var starter = new Starter();
+
+            starter.Run();
         }
     }
 }
